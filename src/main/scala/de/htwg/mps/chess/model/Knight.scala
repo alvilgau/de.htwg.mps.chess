99d22e832082926eac12078c743e5e3955728b78
Alexander Vilgauk <alex@vilgauk.de>
2016-11-02 14:49:47 +0100

    knight finished

--- src/main/scala/de/htwg/mps/chess/model/Knight.scala
+++ src/main/scala/de/htwg/mps/chess/model/Knight.scala
@@ -4,5 +4,8 @@ import de.htwg.mps.chess.model.Team.Team
 
 case class Knight(var posX: Int, var posY: Int, var team: Team) extends Figure {
 
-  override def getPossibleMoves(board: Board) = ???
+  val MOVES = List(Array(-2, 1), Array(-1, 2), Array(1, 2), Array(2, 1),
+    Array(2, -1), Array(1, -2), Array(-1, -2), Array(-2, -1))
+
+  override def getPossibleMoves(board: Board) = simpleMoveValidation(this, board, MOVES)
 }
