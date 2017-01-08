53b56c0b7d34ffaca9346d5f289854116b229b72
Alex Vilgauk <alex@vilgauk.de>
2017-01-08 15:35:59 +0100

    status panel changed font

--- app/core/scala/de/htwg/mps/chess/aview/gui/StatusPanel.scala
+++ app/core/scala/de/htwg/mps/chess/aview/gui/StatusPanel.scala
@@ -1,19 +1,24 @@
 package core.scala.de.htwg.mps.chess.aview.gui
 
-import java.awt.Color
+import java.awt.{Color, Font}
 import javax.swing.BorderFactory
 
 import scala.swing.{GridPanel, Label}
 
 class StatusPanel extends GridPanel(2, 1) {
 
+  val arialFont = new Font("Arial", Font.CENTER_BASELINE, 16)
   background = Color.WHITE
   border = BorderFactory.createLineBorder(Color.BLACK, 1)
 
-  val status = new Label
+  val status = new Label {
+    font = arialFont
+  }
   contents += status
 
-  val turn = new Label
+  val turn = new Label {
+    font = arialFont
+  }
   contents += turn
 
   def setStatus(game: String, checkmate: String): Unit = status.text = "Status: " + game + " " + checkmate
